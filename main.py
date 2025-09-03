ad93cd5aa8b8d41605c1c6f2deb17404a02f2102
Yuval Timen <yuvaltimen@gmail.com>
2025-09-02 22:24:58 -0400

    wip

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ from math import floor
 import pygame
 
 
+
 def main():
     FRAME_RATE = 60
     WALKING_SPEED = 3.4
@@ -86,16 +87,15 @@ def main():
         total_distance_x = ((num_avenues + 1) * AVENUE_BLOCK_LENGTH) + (num_avenues * AVENUE_CROSSWALK_LENGTH)
         total_distance_y = (num_streets + 1) * STREET_BLOCK_LENGTH  + (num_streets * STREET_CROSSWALK_LENGTH)
 
-        print(total_distance_x, total_distance_y)
+        pygame.draw.circle(screen, RED, (0, 0), 10)
+        pygame.draw.circle(screen, RED, (0, screen_height), 10)
+        pygame.draw.circle(screen, RED, (screen_width, 0), 10)
+        pygame.draw.circle(screen, RED, (screen_width, screen_height), 10)
 
         for ave_block_num in range(num_avenues):
-
             percentage_1 = (AVENUE_BLOCK_LENGTH + (ave_block_num * (AVENUE_BLOCK_LENGTH + AVENUE_CROSSWALK_LENGTH))) / total_distance_x
-
             line_1_x = floor(percentage_1 * screen_width)
-
             percentage_2 = (AVENUE_BLOCK_LENGTH + AVENUE_CROSSWALK_LENGTH +  (ave_block_num * (AVENUE_BLOCK_LENGTH + AVENUE_CROSSWALK_LENGTH))) / total_distance_x
-
             line_2_x = floor(percentage_2 * screen_width)
 
             pygame.draw.line(screen, BLACK, (line_1_x, 0), (line_1_x, screen_height), 2)
@@ -104,21 +104,13 @@ def main():
 
         for street_block_num in range(num_streets):
             percentage_1 = (STREET_BLOCK_LENGTH + (street_block_num * (STREET_BLOCK_LENGTH + STREET_CROSSWALK_LENGTH))) / total_distance_y
-
             line_1_y = floor(percentage_1 * screen_height)
-
             percentage_2 = (STREET_BLOCK_LENGTH + STREET_CROSSWALK_LENGTH + (street_block_num * (STREET_BLOCK_LENGTH + STREET_CROSSWALK_LENGTH))) / total_distance_y
-
             line_2_y = floor(percentage_2 * screen_height)
 
             pygame.draw.line(screen, BLACK, (0, line_1_y), (screen_width, line_1_y), 2)
             pygame.draw.line(screen, BLACK, (0, line_2_y), (screen_width, line_2_y), 2)
 
-
-        pygame.draw.circle(screen, RED, (0, 0), 10)
-        pygame.draw.circle(screen, RED, (0, screen_height), 10)
-        pygame.draw.circle(screen, RED, (screen_width, 0), 10)
-        pygame.draw.circle(screen, RED, (screen_width, screen_height), 10)
 
 
         # Update dot position
