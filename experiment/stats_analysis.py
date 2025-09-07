1e243cd0d84d72f0f184cbd4bec36430d211dfd2
Yuval Timen <yuvaltimen@gmail.com>
2025-09-07 11:42:28 -0400

    Migrate to csv

--- experiment/stats_analysis.py
+++ experiment/stats_analysis.py
@@ -4,8 +4,9 @@ import pandas as pd
 
 
 def main():
-    df = pd.read_json("both_biases_experiment.jsonl", lines=True)
+    df = pd.read_csv("both_biases_experiment.csv", index_col=0)
     print(df[["street_policy", "avenue_policy", "green_time", "red_time"]].describe())
+
 
     # Find all the configs with avenue_policy
     mask = (df["street_policy"] - df["avenue_policy"]).abs() >= 0.02 * df["street_policy"]
